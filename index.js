d47d24f604f1a6d2451aff73f2458f14ed1dedac
shakilahmed09 <shakilahmed.pure@gmail.com>
2022-08-16 23:06:29 +0600

    server error solved

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
+const express = require('express');
+const cors = require('cors');
+const app = express();
 const port = process.env.PORT || 5000;
-require('dotenv').config()
+require('dotenv').config();
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
 app.use(cors());
 app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.xvqsjk5.mongodb.net/?retryWrites=true&w=majority`;
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bqjy6zm.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function run() {
     try {
         await client.connect();
-        console.log("Database Connected");
+        console.log("database connected");
 
     }
     finally {
@@ -25,9 +24,10 @@ async function run() {
 }
 run().catch(console.dir)
 
+
 app.get('/', (req, res) => {
-    res.send('running genius server')
+    res.send('running payra server')
 })
 app.listen(port, () => {
-    console.log("Listening to port", port);
+    console.log("payra app listening to port", port);
 })
