2a58404a1b4703475acc61fc8142bbe4e3189406
shakilahmed09 <shakilahmed.pure@gmail.com>
2022-08-24 05:32:07 +0600

    donor request delete api created

--- index.js
+++ index.js
@@ -61,6 +61,12 @@ async function run() {
         /* =======================
             ALL DELETE API
         ======================== */
+        app.delete('/donorRequest/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id)}
+            const result = await bloodDonorCollection.deleteOne(query)
+            res.send(result)
+        })
 
     }
     finally {
